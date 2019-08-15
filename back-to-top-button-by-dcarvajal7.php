985b6703791c6c7d924f0d401530a4fdf3e3646c
dCarvajal7 <danielcarvajalpelicano@gmail.com>
2019-08-15 19:23:31 +0200

    No admin

--- back-to-top-button-by-dcarvajal7.php
+++ back-to-top-button-by-dcarvajal7.php
@@ -11,13 +11,4 @@ License: GPL2
 defined('ABSPATH') or die("Bye bye");
 define('BACK_TO_TOP_BY_DCARVAJAL7_RUTA', plugin_dir_path(__FILE__));
 include(BACK_TO_TOP_BY_DCARVAJAL7_RUTA . 'includes/funciones.php');
-//include(BACK_TO_TOP_BY_DCARVAJAL7_RUTA . 'includes/opciones.php');
-//function loadCSS(){
-//    $css = file_get_contents(BACK_TO_TOP_BY_DCARVAJAL7_RUTA . '/public/css/back-to-top.css', true);
-//    add_option('back-to-top-css',$css,'','yes');
-//}
-//register_activation_hook(__FILE__,'loadCSS');
-//function deactivation(){
-//}
-//register_deactivation_hook(__FILE__,'deactivation');
 ?>
--- uninstall.php
+++ uninstall.php
@@ -3,6 +3,5 @@
 if (!defined('WP_UNINSTALL_PLUGIN')) {
     die;
 }
-$option_name = 'back-to-top-css';
-delete_option($option_name);
+
 ?>
